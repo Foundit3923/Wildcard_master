e7f69f4d51423ec3397abf929422f16e48c96a05
Foundit3923 <30578691+Foundit3923@users.noreply.github.com>
2020-09-06 13:22:01 -0400

    updated to branchless-backwards

--- wildfilter.h
+++ wildfilter.h
@@ -1,7 +1,5 @@
 //
-// Created by Michael Olson and Daniel Davis on 07/05/2020.
-// Moves through a string looking for a wildcard pattern
-// Experiment with overlapping text windows
+// Created by michael on 8/29/20.
 //
 
 #include <stdio.h>
@@ -13,83 +11,77 @@
 #include <time.h>
 #include <math.h>
 
-// Assumes little endian
 
-//---------//
-// GLOBALS //
-//---------//
-
-#define LAST_BITS_ON 72340172838076673     // 00000001 repeated 8 times
+#define LAST_BITS_ON 0x101010101010101UL
+#define ALL_ON 0xFFFFFFFFFFFFFFFFUL
+#define hassetbyte(v) ((~(v) - 0x0101010101010101UL) & (v) & 0x8080808080808080UL)
 
 union Window {
     uint64_t* i;
     char* c;
 }Window;
 
-bool Experimental_wildcard_arbitrary_length_moving_union_1 (char st[],
-                                                            char* subquery_array,
-                                                            uint64_t f_m[]) {
-    //----------//
-    //Processing//
-    //----------//
+union Query {
+    uint64_t* i;
+    char* c;
+};
 
-    char* char_ptr;
-    char* save;
+bool Experimental_wildcard_arbitrary_length_moving_union_save (char* st,
+                                                               char* query_array) {
+    int text_len = (uint64_t) strlen(st);
+    int query_len = (uint64_t) strlen(query_array);
+    int text_modifier = query_len - 1;
+    int shift_count = 0;
 
-    // As text_modifier increases by 8 we move to the next section of the text
-    int text_modifier = 0;
-    int text_len = strlen(st);
+    //char* q.c;
+    char* last;
+    char* string_check[2];
 
     union Window t_w;
-    t_w.c = &st[text_modifier];
+    t_w.c = &st[text_len - text_modifier];
+    
+    union Query q;
+    q.c = &query_array[text_modifier];
 
-    uint64_t subquery_matches = 0;
-    uint64_t encoded_term;
+    uint64_t query_matches = LAST_BITS_ON;
+    uint64_t value;
+    uint64_t int_check[2];
 
-    int subquery_count = 0;
+    bool check = false;
 
-    // Points to beginning of array
-    char_ptr = &subquery_array[subquery_count];
-    save = char_ptr;
+    string_check[0] = &query_array[text_modifier];
 
-    while(char_ptr != '\000' && text_len >= text_modifier) {
-        if(*char_ptr == '*'){
-            char_ptr++;
-            save = char_ptr;
-            subquery_matches = 0;
-            if(!*char_ptr){
-                return true;
-            }
+    int_check[0] = LAST_BITS_ON;
+
+    text_modifier = text_len - text_modifier;
+
+    //t_w.c = "abcaba";
+
+
+    while(text_modifier >= 0) {
+        value = ~((*t_w.i) ^ ((LAST_BITS_ON & query_matches) * (*q.c)));
+
+        check = value > 0;
+
+        if(hassetbyte(value)){
+            query_matches = (((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL & ((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL >> 2)) &
+                            (((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL & ((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL >> 2)) >> 1));
         }
-        encoded_term = ~(f_m[*char_ptr] ^ *t_w.i);
-        if(!subquery_matches) {
-            subquery_matches = LAST_BITS_ON;
+
+        if(&*q.c == &query_array[0]){
+            return true;
         }
-        //Search for matches
-        int match_count;
-        for( match_count = 0; match_count < 8; match_count++){
-            subquery_matches &= encoded_term;
-            encoded_term >>= 1;
-        }
-        if (!subquery_matches) {
-            //no matches, move on
-            text_modifier += 8;
-            if(text_modifier < text_len) {
-                t_w.c = &st[text_modifier];
-                char_ptr = save;
-                subquery_matches = 0;
-            } else {
-                return false;
-            }
-        } else {
-            //char_ptr matches, move to next char_ptr
-            char_ptr++;
-            t_w.c = &st[++text_modifier];
-        }
+
+        int_check[1] = query_matches;
+        query_matches = int_check[check];
+
+        string_check[1] = q.c - check;
+        q.c = string_check[check];
+
+        shift_count += check;
+
+        t_w.c = &st[(text_modifier -= ((8 + shift_count) * !check) + check)];
     }
-    if(text_len >= text_modifier){
-        return true;
-    } else {
-        return false;
-    }
+    return false;
 }
+
