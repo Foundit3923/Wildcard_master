91a706c3100668596914eb9a63d93169d37e513a
Foundit3923 <30578691+Foundit3923@users.noreply.github.com>
2020-09-06 16:22:08 -0400

    added little jump
    calculates the greatest distance that can be jumped without overlap or exceeding the window

--- wildfilter.h
+++ wildfilter.h
@@ -21,6 +21,8 @@ unsigned int c; // store the total here
 static const int S[] = {1, 2, 4, 8, 16}; // Magic Binary Numbers
 static const int B[] = {0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF};
 
+uint64_t long_mask = {0xFFUL,0xFFFFUL,0xFFFFFFUL,0xFFFFFFFFUL,0xFFFFFFFFFFUL,0xFFFFFFFFFFFFUL,0xFFFFFFFFFFFFFFUL,0xFFFFFFFFFFFFFFFFUL};
+
 union Window {
     uint64_t* i;
     char* c;
@@ -38,6 +40,9 @@ int Experimental_wildcard_arbitrary_length_moving_union_save (char* st,
     int text_modifier = query_len - 1;
     int shift_count = 0;
     int result = 0;
+    int distance = 1;
+    int i_check[2];
+    int i;
 
     //char* q.c;
     char* last;
@@ -61,34 +66,45 @@ int Experimental_wildcard_arbitrary_length_moving_union_save (char* st,
 
     text_modifier = text_len - text_modifier;
 
-    //t_w.c = "abcaba";
+    i_check[0] = 1;
 
 
     while(text_modifier >= 0) {
-        value = ~((*t_w.i) ^ ((LAST_BITS_ON & query_matches) * (*q.c)));
+        value = ~((*t_w.i) ^ ((LAST_BITS_ON & query_matches) * (*q.i & long_mask[distance])));
 
         check = value > 0;
 
         if(hassetbyte(value)){
             query_matches = (((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL & ((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL >> 2)) &
                             (((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL & ((value & (value >> 4)) & 0xF0F0F0F0F0F0F0FUL >> 2)) >> 1));
+            distance = __builtin_clz(query_matches);
+            distance = ceil(msb /8);
+            if(distance != 7){
+                for( i=0; i < distance; i++){
+                    if(query_matches & (query_matches >> (i*8))){
+                        distance = i;
+                }
+            }
         }
 
         if(&*q.c == &query_array[0]){
-            query_matches = query_matches - ((query_matches >> 1) & 0x5555555555555555);                          // reuse input as temporary
-            query_matches = (query_matches & 0x3333333333333333) + ((query_matches >> 2) & 0x3333333333333333);   // temp
-            result += ((query_matches + (query_matches >> 4) & 0xF0F0F0FF0F0F0F) * 0x10101011010101) >> 24;       // count
+            resutl = __builtin_popcount(query_matches);
         }
+            
+        i_check[1] = distance;
+        distance = i_check[check];
 
         int_check[1] = query_matches;
         query_matches = int_check[check];
 
-        string_check[1] = q.c - check;
+        string_check[1] = q.c - distance;
         q.c = string_check[check];
+        
+        i_check[0] = 0;
+        i_check[1] = shift_count + distance;    
+        shift_count = i_check[count];
 
-        shift_count += check;
-
-        t_w.c = &st[(text_modifier -= ((8 + shift_count) * !check) + check)];
+        t_w.c = &st[(text_modifier -= ((8 + shift_count) * !check) + (distance * check))];
     }
     return result;
 }
